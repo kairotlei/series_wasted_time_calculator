a9e8dea5edab4df964ffd328c84fdce406fbf927
Your name <juanke234@gmail.com>
2020-11-25 01:45:41 +0100

    Add files via upload

---
+++ main_english.py
@@ -0,0 +1,39 @@
+import webbrowser
+import time
+
+time_start = float(time.time())
+print("Clear your mind and think on a series...")
+
+seasons = int(input("How many seasons does it have? "))
+chapters = int(input("How many chapters does a season have? "))
+mins_per_ch = int(input("How long is every chapter (in minutes)? "))
+previously_duration = int(input("How long is the 'previously...' when a chapter starts (in seconds)? "))
+intro_duration = int(input("How long is the series intro just before the chapter starts (in seconds)? "))
+credits_length = int(input("How long are the final credits (in seconds)? "))
+series_name = str(input("Oh, what is the series name? "))
+
+wasted_time_hrs = round(float((mins_per_ch * chapters * seasons) / 60), 2)
+save_previous_mins = round(float(((previously_duration * chapters * seasons) / 60)), 2)
+save_intro_mins = round(float(((intro_duration * chapters * seasons) / 60)), 2)
+save_outro_mins = round(float(((credits_length * chapters * seasons) / 60)), 2)
+total_cha = round(int(seasons * chapters), 2)
+total_savings = round(int(save_intro_mins + save_outro_mins + save_outro_mins), 2)
+good_series_hrs = round(int((save_intro_mins + save_outro_mins + save_outro_mins) / wasted_time_hrs), 2)
+
+print(f"""
+Congratulations, watching '{series_name}' completely you would waste {wasted_time_hrs} hours of your life, distributed in {seasons} seasons with a total of {total_cha} chapters!
+Are you miserly? You can save {save_previous_mins} minutes skipping the 'previously...' summary when a chapter starts.
+If you skip the intro of every chapter, you would recover another {save_intro_mins} minutes of you life time.
+You could also save {save_outro_mins} minutes if you don't stare at the ending credits.
+
+So, in conclusion:
+- Watching '{series_name}' would cost you {wasted_time_hrs} irrecoverable hours of you life.
+- Ignoring flourishes (summary, intro, credits) saves you {total_savings} minutes.
+- The {good_series_hrs}% of '{series_name}' is worthless time (% is flourishes time / all series time)
+""")
+time_end = float(time.time())
+time_final = round(float(time_end - time_start), 2) 
+print(f"Also, did you notice that you just wasted another {time_final} seconds while doing this test?.")
+
+# Opens a Twitter window where you can tweet and comment you result.
+webbrowser.open(f"""https://twitter.com/intent/tweet?text=Hey, if I completely watch the '{series_name}' series, I would waste {wasted_time_hrs} hours of my life into it. Ignoring flourishes I could recover {total_savings} minutes! And I also wasted {time_final} seconds while discovering this!!""")
---
+++ main_spanish.py
@@ -0,0 +1,39 @@
+import webbrowser
+import time
+
+time_start = float(time.time())
+print("Piensa en una serie...")
+
+seasons = int(input("¿Cuántas temporadas tiene? "))
+chapters = int(input("¿Cuántos capítulos hay en cada temporada? "))
+mins_per_ch = int(input("¿Cuántos minutos dura, en total y sin decimales, cada capitulo? "))
+previously_duration = int(input("¿Duración, en segundos y sin decimales, del 'anteriormente...' antes de cada capítulo? "))
+intro_duration = int(input("¿Duración, en segundos, de la intro de cada capítulo? "))
+credits_length = int(input("¿Cuánto duran los créditos finales? "))
+series_name = str(input("Ah, ¿y cómo se llama la serie? "))
+
+wasted_time_hrs = round(float((mins_per_ch * chapters * seasons) / 60), 2)
+save_previous_mins = round(float(((previously_duration * chapters * seasons) / 60)), 2)
+save_intro_mins = round(float(((intro_duration * chapters * seasons) / 60)), 2)
+save_outro_mins = round(float(((credits_length * chapters * seasons) / 60)), 2)
+total_cha = round(int(seasons * chapters), 2)
+total_savings = round(int(save_intro_mins + save_outro_mins + save_outro_mins), 2)
+good_series_hrs = round(int((save_intro_mins + save_outro_mins + save_outro_mins) / wasted_time_hrs), 2)
+
+print(f"""
+¡Felicidades, al ver la serie '{series_name}' de principio a fin habrías perdido {wasted_time_hrs} horas de tu vida distribuídas en {seasons} temporadas con un total de {total_cha} capítulos!
+Si te va el tacañismo, puedes ahorrarte {save_previous_mins} minutos saltándote el resumen de lo ocurrido anteriormente.
+Y si te saltaras la misma intro de cada capítulo recuperarías {save_intro_mins} minutos de tu vida.
+También podrías rescatar otros {save_outro_mins} minutos si ignoraras la palabrería que sale al final de cada capítulo.
+
+En conclusión:
+- Ver '{series_name}' te costará {wasted_time_hrs} horas de tu vida.
+- Podrías recuperar {total_savings} minutos ignorando florituras.
+- El {good_series_hrs}% de '{series_name}' es tiempo tirado a la basura (contenido no relevante en relación a contenido total)
+""")
+time_end = float(time.time())
+time_final = round(float(time_end - time_start), 2) 
+print(f"Por cierto, al hacer este test has perdido unos preciosos {time_final} de tu vida.")
+
+# Abre una ventana de Twitter en donde poder tweetear y comentar el resultado
+webbrowser.open(f"""https://twitter.com/intent/tweet?text=Hey, si viera la serie '{series_name}' completamente, perderia {wasted_time_hrs} horas de mi vida en ella. Ignorando florituras podría recuperar {total_savings} minutos! ¡¡Y he desperdiciado {time_final} segundos en este test!!""")
